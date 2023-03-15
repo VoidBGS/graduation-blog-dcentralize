4b58dda893afe4b82d12cad99c7bb3165a7e6cfb
kristianRL <kristian@d-centralize.nl>
2023-03-16 01:18:32 +0200

    smooth scroll

--- scripts/scrollToTop.js
+++ scripts/scrollToTop.js
@@ -1,16 +1,20 @@
-let scrollButton = document.getElementById("fast-scroll");
+const scrollButton = document.getElementById("fast-scroll");
+const SCROLL_TRIGGER = 300;
 
-const scrollToTop = (e) =>{
-    e.preventDefault();
-    window.scrollTo(0,0);
+const scrollToTop = (e) => {
+    e?.preventDefault();
+    if(window.scrollY < 1)
+        return;
+    window.scrollTo(0, Math.floor(window.scrollY) * 0.95);
+    setTimeout(scrollToTop, 10);
 }
 
-const handleWindowScroll = (e) =>{
+const handleWindowScroll = (e) => {
     e.preventDefault();
-    if (window.scrollY > 300)
+    if (window.scrollY > SCROLL_TRIGGER)
         scrollButton.style.right = "1em";
-    else 
-        scrollButton.style.right = "-100em";
+    else
+        scrollButton.style.right = "-20em";
 }
 
 scrollButton.addEventListener('click', scrollToTop);
