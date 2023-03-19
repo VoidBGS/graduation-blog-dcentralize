9f8e0502447886142b458bb4142f6a90be20c992
kristianRL <kristian@d-centralize.nl>
2023-03-19 17:11:23 +0200

    added local storage for color saving

--- scripts/fontColor.js
+++ scripts/fontColor.js
@@ -1,11 +1,25 @@
-let root = document.querySelector(':root');
-let funBtn = document.getElementById("fn-button");
+const root = document.querySelector(':root');
+const funBtn = document.getElementById("fn-button");
+const STORAGE_NAME = "font-color";
 
 function changeColor(root) {
-    let randColors = ["pink", "violet", "white", "red", "fuchsia", "cyan", "orange", "#4ed842"];
-    let randNumber = Math.floor(Math.random() * 7);
-    let currentColor = root.style.getPropertyValue('--primary');
-    root.style.setProperty('--primary', randColors.filter(color => color !== currentColor)[randNumber]);
+    const randColors = ["pink", "violet", "white", "red", "fuchsia", "cyan", "orange", "#4ed842"];
+    const randNumber = Math.floor(Math.random() * 7);
+    const currentColor = root.style.getPropertyValue('--primary');
+    const randomColor = randColors.filter(color => color !== currentColor)[randNumber];
+    addColorInStorage(randomColor);
+    root.style.setProperty('--primary', randomColor);
+}
+
+function addColorInStorage(color) {
+    localStorage.setItem(STORAGE_NAME, color);
+}
+
+function loadColorFromStorage(event) {
+    event.preventDefault();
+    const loadedColor = localStorage.getItem(STORAGE_NAME);
+    root.style.setProperty('--primary', loadedColor);
 }
 
 funBtn.addEventListener('click', () => changeColor(root));
+document.addEventListener("DOMContentLoaded", loadColorFromStorage);
